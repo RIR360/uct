8629893d0c345b35d92fe397dd4ed47b0c909492
Rejwan Islam Rizvy <50569315+RIR360@users.noreply.github.com>
2021-12-14 21:01:06 +0600

    Update 2020 - Section B.c

--- SPL/2020-lab/2020 - Section B.c
+++ SPL/2020-lab/2020 - Section B.c
@@ -1,9 +1,11 @@
+/*
 >>>>> 2020 <<<<<
 >>>>> Section B <<<<<
 >>>>> Written by: RIR360 <<<<<
+*/
 
 
-(1) Multiply two matrices
+// (1) Multiply two matrices
 
 #include <stdio.h>
 
@@ -71,8 +73,8 @@ int main()
 }
 
 
-(2) Evaluate the expression using user defined functions.
-Series: x - x^3/3! + x^5/5! - x^7/7! ...
+// (2) Evaluate the expression using user defined functions.
+// Series: x - x^3/3! + x^5/5! - x^7/7! ...
 
 #include <stdio.h>
 
@@ -102,6 +104,7 @@ float calculate(int x, int n)
         else
             sum += x^(2 * i - 1)/factorial(2 * i - 1);
     }
+    return sum;
 }
 
 int factorial(int n)
@@ -115,7 +118,7 @@ int factorial(int n)
 }
 
 
-(3) Using array of structures read the number of three subjects for n students and sort them according to their position.
+// (3) Using an array of structures read the number of three subjects for n students and sort them according to their position.
 
 #include <stdio.h>
 
@@ -176,11 +179,170 @@ int main()
 }
 
 
+// (4) Calculate the sum of the numbers of an array using pointers
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+    
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    int *ptr = arr;
+    
+    printf("Enter the Elements of array: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    for(i = 0; i < n; i++)
+    {
+        sum += *ptr;
+        ptr++;
+    }
+    printf("Sum of the array: %d\n", sum);
+    return 0;
+}
 
 
+// (5) Copy the elements of one file into another
+
+#include <stdio.h>
+
+int main()
+{
+    char mainfn[100], copyfn[100];
+    
+    printf("Enter the file to copy: ");
+    scanf("%s", mainfn);
+    
+    FILE *mainfile = fopen(mainfn, "r");
+    if (mainfile == NULL)
+    {
+        printf("Can't read %s.\n", mainfn);
+        return 1;
+    }
+    
+    printf("Enter the name of copy file: ");
+    scanf("%s", copyfn);
+    
+    FILE *copyfile = fopen(copyfn, "w");
+    if (copyfile == NULL)
+    {
+        printf("Can't write %s.\n", copyfile);
+        return 1;
+    }
+    
+    char ch;
+    do {
+        ch = fgetc(mainfile);
+        fprintf(copyfile, "%c", ch);
+    } while (ch != EOF);
+    
+    fclose(mainfile);
+    fclose(copyfile);
+    
+    printf("File copied as %s.\n", copyfn);
+    
+    return 0;
+}
 
 
+// (6) Read data from keyboard write it to the INPUT.txt file, read data from the same file and print to the console
 
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    
+    char text[1000], ch;
 
+    FILE *fptr1 = fopen("INPUT.txt", "w");
+    if (fptr1 == NULL) {
+        printf("Can't write a file.\n");
+        return 1;
+    }
+    
+    printf("Enter a text: ");
+    gets(text);
+    
+    fprintf(fptr1, text);
+    fclose(fptr1);
 
+    FILE *fptr2 = fopen("INPUT.txt","r");
+    if(fptr2 == NULL)
+    {
+        printf("Can't read INPUT file.\n");
+        return 1;
+    }
+
+    do
+    {
+        ch = fgetc(fptr2);
+        putchar(ch);
+    } while(ch != EOF);
+
+    fclose(fptr2);
+    return 0;
+}
+
+// (7) store the following data into the INVENTORY file. then read the file again and display the values
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    
+    int i, n, number, quantity;
+    float price;
+    char name[20], ch;
+
+    FILE *fptr1 = fopen("INVENTORY.txt", "w");
+    if (fptr1 == NULL) {
+        printf("Can't write a file.\n");
+        return 1;
+    }
+    fprintf(fptr1, "Item Name\tNumber\tPrice\tQuantity\n");
+    
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+    
+    for (i = 0; i < n; i++)
+    {
+        printf("\nItem-%d info.\n", i + 1);
+        printf("Item Name: ");
+        scanf("%s", name);
+        printf("Number: ");
+        scanf("%d", &number);
+        printf("Price: ");
+        scanf("%f", &price);
+        printf("quantity: ");
+        scanf("%d", &quantity);
+        
+        fprintf(fptr1, "%s\t\t%d\t%2.f\t%d\n", name, number, price, quantity);
+    }
+    
+    printf("\nWritten file successfully.\n\n");
+    fclose(fptr1);
+    
+    printf("YOUR INVENTORY:\n");
+    FILE *fptr2 = fopen("INVENTORY.txt", "r");
+    if (fptr2 == NULL)
+    {
+        printf("Can't read INVENTORY file.\n");
+        return 1;
+    }
+    
+    do {
+        ch = fgetc(fptr2);
+        printf("%c", ch);
+    } while (ch != EOF);
+    
+    fclose(fptr2);
+    return 0;
+}
